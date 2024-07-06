43433fbce359b347f2da5d0e6a24c09bbbcccb2f
nonamie <53796817+mathfox@users.noreply.github.com>
2024-07-06 20:25:08 +0300

    mvp

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+export declare const MAX_SAFE_INTEGER = 9007199254740991
+export declare const MIN_SAFE_INTEGER = -9007199254740991
+
+export declare function isFinite(value: unknown): boolean
+
+export declare function isInteger(value: unknown): boolean
+
+export declare function isNaN(value: unknown): boolean
+
+export declare function isSafeInteger(value: unknown): boolean
